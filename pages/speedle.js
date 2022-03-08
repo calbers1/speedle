3b85c5b2ddaf20053656f807f16abf216be8d094
Connor Albers <calbers1@harding.edu>
2022-03-08 15:43:46 -0600

    add logout

--- pages/speedle.js
+++ pages/speedle.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import { Button, Container } from "@mui/material";
+import { Box } from "@mui/system";
 
 export default function Speedle() {
 	const [userName, setUserName] = useState("");
@@ -11,10 +13,27 @@ export default function Speedle() {
 		sethighScore(window.sessionStorage.getItem("highScore"));
 		setaverageScore(window.sessionStorage.getItem("averageScore"));
 	}, []);
+
+	const logOut = () => {
+		window.localStorage.removeItem("userString");
+		window.sessionStorage.removeItem("userName");
+		window.sessionStorage.removeItem("highScore");
+		window.sessionStorage.removeItem("averageScore");
+		router.push("/");
+	};
 	return (
-		<h1>
-			Logged In As {userName}. Average Score: {averageScore}. High Score:{" "}
-			{highScore}
-		</h1>
+		<Container>
+			<Box>
+				Logged In As {userName}. Average Score: {averageScore}. High Score:
+				{highScore}.
+			</Box>
+			<Button
+				variant="contained"
+				onClick={logOut}
+				sx={{ width: "100%", marginTop: "1em" }}
+			>
+				Log Out
+			</Button>
+		</Container>
 	);
 }
