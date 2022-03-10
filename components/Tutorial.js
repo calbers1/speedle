ce44e70c97ece47a60c92201dbfeadc4fe975f5d
Connor Albers <calbers1@harding.edu>
2022-03-10 08:35:57 -0600

    fix for mobile

--- components/Tutorial.js
+++ components/Tutorial.js
@@ -21,18 +21,6 @@ export default function Tutorial() {
 					</Grid>
 					<Grid className="grid" item xs={12}>
 						<p className="Tutorial-text">
-							Do you want to play a casual, laid back word-finding game, and
-							post little squares on social media? <h2>TOO BAD.</h2>
-						</p>
-						<p className="Tutorial-text">
-							This is a game about being smarter and faster than everyone else.
-							The gameplay is similar to a popular (read: sellout) word game,
-							but definitely WAYYYY different, because I added a timer. Your
-							score is calculated by how many tries are left and how much time
-							you take to get it right. Then, your score goes on a global
-							leaderboard, for everyone to see. <h3>Welcome to Not-Wordle.</h3>
-						</p>
-						<p className="Tutorial-text">
 							The aim of the game is to guess the word. You have 6 tries and 60
 							seconds. After each guess, the you can see which letters you got
 							in the correct spot (green), which letters are correct but in the
