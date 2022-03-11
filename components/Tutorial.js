de379464542e1f260c40eb305bbebb9cbed739ab
Connor Albers <calbers1@harding.edu>
2022-03-11 14:24:09 -0600

    fixed &apos; again

--- components/Tutorial.js
+++ components/Tutorial.js
@@ -21,11 +21,11 @@ export default function Tutorial() {
 					</Grid>
 					<Grid className="grid" item xs={12}>
 						<p className="Tutorial-text">
-							The aim of the game is to guess the word. Here's the catch: You
-							have 6 tries and 60 seconds, so you'll need to blurt it out fast.
-							After each guess, the you can see which letters you got in the{" "}
-							<span className="Tutorial-correct"> correct spot</span>, which
-							letters are
+							The aim of the game is to guess the word. Here&apos;s the catch:
+							You have 6 tries and 60 seconds, so you&apos;ll need to blurt it
+							out fast. After each guess, the you can see which letters you got
+							in the <span className="Tutorial-correct"> correct spot</span>,
+							which letters are
 							<span className="Tutorial-wrong-spot">
 								{" "}
 								correct but in the wrong spot
@@ -35,7 +35,8 @@ export default function Tutorial() {
 							USE THESE HINTS. Once you finish (or run out of time) your score
 							is calculated and thrown on the leaderboard next to your name for
 							everyone to see. You get a bonus multiplier for having a streak,
-							so check back every day or you'll lose it! <h3>Good luck!</h3>
+							so check back every day or you&apos;ll lose it!{" "}
+							<h3>Good luck!</h3>
 						</p>
 					</Grid>
 					<Grid className="grid" item xs={12}>
