ee42db5ac8267b7a8584a32b44c785469bca8c0a
Connor Albers <calbers1@harding.edu>
2022-03-19 18:41:39 -0500

    add number to Leaderboard and fix wrong-word size

--- pages/Leaderboard.js
+++ pages/Leaderboard.js
@@ -1,48 +1,81 @@
-import { useRouter } from "next/router";
-import { Button, TextField, Grid, Box, Table, TableBody, TableCell, TableHead, TableRow, TableContainer, Paper } from "@mui/material";
-import { useEffect, useState } from "react";
+import { useRouter } from 'next/router'
+import {
+	Button,
+	TextField,
+	Grid,
+	Box,
+	Table,
+	TableBody,
+	TableCell,
+	TableHead,
+	TableRow,
+	TableContainer,
+	Paper,
+} from '@mui/material'
+import { useEffect, useState } from 'react'
 
-export default function Leaderboard(){
-    const [scores, setScores] = useState([]);
-    
-	const router = useRouter();
+export default function Leaderboard() {
+	const [scores, setScores] = useState([])
 
-    useEffect(async ()=>{
-        const _scores = await (await fetch('/api/getScores')).json();
-        setScores(_scores);
-    },[])
+	const router = useRouter()
 
-    return(
-        <div>
-            <h1 className="scoreLabel">Today&apos;s Scores</h1>
-        <TableContainer component={Paper} sx={{minWidth: "80vw", maxHeight: "60vh"}}>
+	useEffect(async () => {
+		const _scores = await (await fetch('/api/getScores')).json()
+		setScores(_scores)
+	}, [])
 
-        <Table sx={{minWidth: "80vw"}} size="large" stickyHeader>
-            <TableHead>
-                <TableRow>
-                    <TableCell className="columnLabel">Username</TableCell>
-                    <TableCell className="columnLabel" align="center">Score</TableCell>
-                    <TableCell className="columnLabel" align="center">Streak</TableCell>
-                </TableRow>
-            </TableHead>
-            <TableBody>
-
-            {
-                scores !== [] ? scores.map((score, scoreIndex)=>(
-                    <TableRow key={scoreIndex} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                        <TableCell className="scoreCell">
-                            {score.userName}
-                        </TableCell>
-                        <TableCell className="scoreCell" align="center">{score.score}</TableCell>
-                        <TableCell className="scoreCell" align="center">{score.streak}</TableCell>
-                    </TableRow>
-                    )) : <></>
-                }
-        
-                </TableBody>
-            </Table>
-                </TableContainer>
-                <Button className="btnNav" variant="outlined" onClick={()=>{router.push(`blurtle`)}}>Back To Blurtle</Button>
-                </div>
-    )
+	return (
+		<div>
+			<h1 className="scoreLabel">Today&apos;s Scores</h1>
+			<TableContainer
+				component={Paper}
+				sx={{ minWidth: '80vw', maxHeight: '60vh' }}
+			>
+				<Table sx={{ minWidth: '80vw' }} size="large" stickyHeader>
+					<TableHead>
+						<TableRow>
+							<TableCell className="columnLabel">Username</TableCell>
+							<TableCell className="columnLabel" align="center">
+								Score
+							</TableCell>
+							<TableCell className="columnLabel" align="center">
+								Streak
+							</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{scores !== [] ? (
+							scores.map((score, scoreIndex) => (
+								<TableRow
+									key={scoreIndex}
+									sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+								>
+									<TableCell className="scoreCell">
+										<b>{scoreIndex + 1}.</b> &nbsp; {score.userName}
+									</TableCell>
+									<TableCell className="scoreCell" align="center">
+										{score.score}
+									</TableCell>
+									<TableCell className="scoreCell" align="center">
+										{score.streak}
+									</TableCell>
+								</TableRow>
+							))
+						) : (
+							<></>
+						)}
+					</TableBody>
+				</Table>
+			</TableContainer>
+			<Button
+				className="btnNav"
+				variant="outlined"
+				onClick={() => {
+					router.push(`blurtle`)
+				}}
+			>
+				Back To Blurtle
+			</Button>
+		</div>
+	)
 }
