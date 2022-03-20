dd2ca99ebe6fd322bef1d23d522afb2da3567cec
Connor Albers <calbers1@harding.edu>
2022-03-20 00:59:50 -0500

    fix login bug

--- pages/CreateUserForm.js
+++ pages/CreateUserForm.js
@@ -1,73 +1,67 @@
-import { useRouter } from "next/router";
-import { Button, TextField, Grid, Box, Container } from "@mui/material";
+import { useRouter } from 'next/router'
+import { Button, TextField, Grid, Box, Container } from '@mui/material'
 export default function CreateUserForm() {
-	const router = useRouter();
+	const router = useRouter()
 	const handleSubmit = async (event) => {
-		event.preventDefault();
+		event.preventDefault()
 
-		const form = new FormData(event.target);
+		const form = new FormData(event.target)
 
-		const formData = Object.fromEntries(form.entries());
+		const formData = Object.fromEntries(form.entries())
 
-		const params = new URLSearchParams(formData);
+		const params = new URLSearchParams(formData)
 
-		if (formData.userName.trim() === "" || formData.pass.trim() === "") {
-			alert("Enter a username and password.");
-			return;
+		if (formData.userName.trim() === '' || formData.pass.trim() === '') {
+			alert('Enter a username and password.')
+			return
 		}
 
-		const isValid = await (
-			await fetch("/api/isValidUsername?" + params)
-		).json();
+		const isValid = await (await fetch('/api/isValidUsername?' + params)).json()
 
 		if (isValid === 0 && formData.pass === formData.pass2) {
 			if (formData.pass.trim().length >= 8) {
-				const res = await fetch("/api/createUser", {
+				const res = await fetch('/api/createUser', {
 					body: JSON.stringify(formData),
 					headers: {
-						"content-type": "multipart/form-data",
+						'content-type': 'multipart/form-data',
 					},
-					method: "POST",
-				});
-				const userString = await res.text();
-				window.localStorage.setItem("userString", userString);
+					method: 'POST',
+				})
+				const userString = await res.text()
+				window.localStorage.setItem('userString', userString)
 
 				if (userString) {
 					const q = {
 						userID: userString,
-					};
-
-					const params = new URLSearchParams(q);
-
-					try {
-						const res = await (
-							await fetch("/api/BypassLogIn?" + params)
-						).json();
-						const user = res.user;
-						if (!res.error) {
-							window.sessionStorage.setItem("showTutorial", 1);
-						}
-					} catch (err) {
-						console.error("ERROR: ", err);
 					}
 
-					router.push(`blurtle`);
-				} else alert("Failed to create user.");
+					const params = new URLSearchParams(q)
+
+					try {
+						const res = await (await fetch('/api/BypassLogIn?' + params)).json()
+						const user = res.user
+						if (!res.error) {
+							window.sessionStorage.setItem('showTutorial', 1)
+						}
+					} catch (err) {
+						console.error('ERROR: ', err)
+					}
+
+					router.push(`blurtle`)
+				} else alert('Failed to create user.')
 			} else {
 				alert(
 					"Do you want your account stolen? Because that's how you get your account stolen. Password must be at least 8 characters."
-				);
+				)
 			}
+		} else if (formData.pass !== formData.pass2) {
+			alert(
+				"Passwords do not match, and I'm too lazy to let you see them (for now)."
+			)
 		} else {
-			if (formData.pass !== formData.pass2) {
-				alert(
-					"Passwords do not match, and I'm too lazy to let you see them (for now)."
-				);
-			} else {
-				alert("That username is already taken. Please try another user name.");
-			}
+			alert('That username is already taken. Please try another user name.')
 		}
-	};
+	}
 
 	return (
 		<div>
@@ -113,7 +107,7 @@ export default function CreateUserForm() {
 							/>
 						</Grid>
 						<Grid item xs={12} md={12}>
-							<Button variant="contained" type="submit" sx={{ width: "100%" }}>
+							<Button variant="contained" type="submit" sx={{ width: '100%' }}>
 								Register
 							</Button>
 						</Grid>
@@ -121,5 +115,5 @@ export default function CreateUserForm() {
 				</div>
 			</Box>
 		</div>
-	);
+	)
 }
